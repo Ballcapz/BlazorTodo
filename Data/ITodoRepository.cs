faa53e83f25a4df0e2e49c10fb45f2a2d012b6b7
Zach Johnson <zjohnson@omnitech-inc.com>
2020-06-09 09:51:43 -0500

    Blazor version of simple todo

---
+++ Data/ITodoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTodo.Data
+{
+    public interface ITodoRepository
+    {
+        Task<IEnumerable<Todo>> GetAll();
+        Task Add(Todo todo);
+        Task Remove(Guid id);
+        Task<Todo> Find(Guid id);
+        Task Update(Todo todo);
+    }
+}
---
+++ Data/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTodo.Data
+{
+    public class Todo
+    {
+        public Todo()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
